399cc7357bced5253df39fdc819d3c02c21467e5
flamboyant-ryan <ryanbabatunde777@gmail.com>
2022-01-02 23:33:59 +0100

    forgot to update contract
    /
    '

--- contract/create.sol
+++ contract/create.sol
@@ -27,11 +27,12 @@ contract RPGees is ERC721{
     address internal _owner;
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
     uint offset = 1000000000000000000;
-    uint public mint_price = 3 * offset;
+    uint public mint_price = 2 * offset;
     IERC20Token ERC = IERC20Token(cUsdTokenAddress);
 
-    uint8 PHYSICAL = 1;
-    uint8 MAGIC = 2;
+    uint PHYSICAL = 0;
+    uint MAGIC = 1;
+    uint NONCE = 7;
 
     struct Character{
         string weapon;
@@ -42,7 +43,7 @@ contract RPGees is ERC721{
         uint effectiveness;
     }
     mapping (uint => Character) internal characters;
-
+    uint token_supply;
     
     string[] physical_items = ["sword", "mace", "gauntlets", "war hammer"];
     string[] magic_items = ["staff", "orb", "wand" ,"book" ];
@@ -56,8 +57,9 @@ contract RPGees is ERC721{
             "of healing",
             "of the people"
             ];
-    string[] titles = ["mage", "scholar", "bezerker", "warrior"];
-    string[] prepends = ["enchanted", "fire", "glorious", "weird", "flimsy", "necro", "chakra", "shiny"];
+    string[] physical_titles = ["Warrior", "Bezerker"];
+    string[] magical_titles = ["Mage", "Scholar"];
+    string[] prepends = ["Enchanted", "Fire", "Glorious", "Weird", "Flimsy", "Necro", "Chakra", "Shiny"];
     string[] clothing = [
             "pirate tunics",
             "smart tunics",
@@ -82,23 +84,23 @@ contract RPGees is ERC721{
         _;
     }
 
-    function _get_random(uint nonce) public view returns (uint256) {
+    function _get_random(uint nonce) private view returns (uint256) {
         return uint(keccak256(abi.encodePacked(block.timestamp, nonce)));
     }
 
-    function _get_random_prepend(uint nonce) public view returns (string memory){
+    function _get_random_prepend(uint nonce) private view returns (string memory){
         return prepends[_get_random(nonce) % prepends.length];
     }
 
-    function _get_random_append(uint nonce) public view returns (string memory){
+    function _get_random_append(uint nonce) private view returns (string memory){
         return appends[_get_random(nonce) % appends.length];
     }
 
-    function _get_random_clothing(uint nonce) public view returns (string memory){
+    function _get_random_clothing(uint nonce) private view returns (string memory){
         return clothing[_get_random(nonce) % clothing.length];
     }
 
-    function _assign_weapon(uint _type) public view returns (string memory){
+    function _assign_weapon(uint _type) private view returns (string memory){
         string memory weapon = "";
         string memory pre = "";
         string memory post = "";
@@ -121,66 +123,68 @@ contract RPGees is ERC721{
         if (_type == MAGIC){
             weapon = magic_items[_get_random(_type) % magic_items.length];
 
-        }else{
+        }else if (_type == PHYSICAL){
             weapon = physical_items[_get_random(_type) % physical_items.length];
         }
+        else{revert();}
 
         return string(abi.encodePacked(pre, " ", weapon, " ", post));
 
     }
 
-    function mint_character(string memory name) public returns (uint256, uint256) {
+    function assign_name(string memory name, uint _type) private view returns (string memory){
+        string memory char_name = "";
+        if (_type == MAGIC){
+            if (! ((_get_random(NONCE) % 1000) == 7)){
+                char_name = string(abi.encodePacked(magical_titles[_get_random(NONCE) % magical_titles.length]," ", name));
+            }
+            // a one in a thousand chance of having a magic type normal character
+            else{
+                char_name = string(abi.encodePacked(physical_titles[_get_random(NONCE) % physical_titles.length]," ", name));
+            }
+        }
 
-        // mint a new character by first
-        // require(
-        //     ERC.transferFrom(
-        //         msg.sender,
-        //         _owner,
-        //         mint_price
-        //     ),
-        //     "Could not initialiate transfer"
-        // );
+        if (_type == PHYSICAL){
+            if (! ((_get_random(NONCE) % 1000) == 7)){
+                char_name = string(abi.encodePacked(physical_titles[_get_random(NONCE) % physical_titles.length]," ", name));
+            }
+            // a one in a thousand chance of having a normal type magic character
+            else{
+                char_name = string(abi.encodePacked(magical_titles[_get_random(NONCE) % magical_titles.length]," ", name));
+            }
+        }
+        return char_name;
+    }
+
+    function mint_character(string memory name) public returns (uint256) {
+
+        // mint a new character by first paying money
+        require(
+            ERC.transferFrom(
+                msg.sender,
+                _owner,
+                mint_price
+            ),
+            "Could not initialiate transfer"
+        );
 
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
 
-        uint nonce = uint(keccak256(abi.encodePacked(name)));
 
-        uint _type = (_get_random(nonce) % 2) + 1;
+        uint _type = _get_random(NONCE) % 2;
 
+        NONCE = NONCE + (_get_random(NONCE) % 100);
 
-        string memory char_name;
-        string memory weapon;
+        string memory char_name = assign_name(name, _type);
+        string memory weapon = _assign_weapon(_type);
 
-        if (_type == MAGIC){
-            if (! (_get_random(nonce) % 1000 == 7)){
-                char_name = string(abi.encode(titles[_get_random(nonce) % 2]," ", name));
-            }
-            // a one in a thousand chance of having a magic type normal character
-            else{
-                char_name = string(abi.encode(titles[(_get_random(nonce) % 2) + 2]," ", name));
-
-            }
-            weapon = _assign_weapon(MAGIC);
-        }
-
-        if (_type == PHYSICAL){
-            if (! (_get_random(nonce) % 1000 == 7)){
-                char_name = string(abi.encode(titles[(_get_random(nonce) % 2) + 2]," ", name));
-            }
-            // a one in a thousand chance of having a normal type magic character
-            else{
-                char_name = string(abi.encode(titles[_get_random(nonce) % 2]," ", name));
-            }
-            weapon = _assign_weapon(PHYSICAL);
-        }
-
-        string memory char_clothing = _get_random_clothing(nonce);
+        string memory char_clothing = _get_random_clothing(NONCE);
 
         // all characters start with a random effectiveness assigned, capped at 20
         // effectiveness helps in fights
-        uint effectiveness = _get_random(nonce) % 20;
+        uint effectiveness = _get_random(NONCE) % 20;
 
         // add character to mapping and mint
         characters[newItemId] = Character(
@@ -192,18 +196,39 @@ contract RPGees is ERC721{
             effectiveness
         );
         _mint(msg.sender, newItemId);
-        
-        return (newItemId, nonce);
+        return (newItemId);
     }
 
     function change_mint_price(uint newprice) only_owner public {
         mint_price = newprice * offset;
     }
 
-    function fight(uint challenger_id, uint challenged_id) public {
+    function getTotalTokenSupply() public view returns (uint) {
+        return _tokenIds.current();
     }
 
-   
+    function getCharDetails(uint token_id) public view returns (
+        string memory,
+        string memory,
+        string memory,
+        uint,
+        uint,
+        uint,
+        address
+        // address
+    ){
+        Character storage chr = characters[token_id];
+        return (
+        chr.weapon,
+        chr.clothing,
+        chr.name,
+        chr.battles_won,
+        chr._type,
+        chr.effectiveness,
+        ownerOf(token_id)
+        );
+    }
+
   
     function tokenURI(uint256 token_id) override public view returns (string memory) {
         
@@ -212,6 +237,45 @@ contract RPGees is ERC721{
             "'effectiveness': '", char.effectiveness,  "', 'type': '", char._type == MAGIC ? "Magic type": "physical type",
             "'weapon': '", char.weapon, "'clothing': '", char.clothing,  "'}"));
         return string(abi.encodePacked("data:application/json,", json));
+    }
+
+    function fight(uint challenger_id, uint challenged_id) public returns (uint){
+        
+        require(ownerOf(challenger_id) == msg.sender);
+        require(challenger_id != challenged_id);
+        uint totalodds = 100000000;
+        
+        // halfway point of total odds space
+        uint fifty_fifty = totalodds / 2;
+
+
+        // check for which character has effectiveness advantage
+        bool challenger_has_advantage = true;
+        if (characters[challenger_id].effectiveness < characters[challenged_id].effectiveness){
+            challenger_has_advantage = false;
+        }
+
+        uint advantage;
+        uint winner;
+
+        // use the effectiveness advantage to skew the odds in the person's with the advantages favour
+        if (challenger_has_advantage){
+            advantage = characters[challenger_id].effectiveness - characters[challenged_id].effectiveness;
+            if ((_get_random(advantage) % totalodds) <  fifty_fifty + advantage){
+                winner = challenger_id;
+            }else winner = challenged_id;
+        }
+        else {
+            advantage =  characters[challenged_id].effectiveness - characters[challenger_id].effectiveness;}
+            if ((_get_random(advantage) % totalodds) <  fifty_fifty + advantage){
+                winner = challenged_id;
+            }else winner = challenger_id;
+        
+        // increase the effectiveness of the winner by a random amount capped at 100
+        characters[winner].effectiveness += _get_random(advantage) % 100;
+        characters[winner].battles_won += 1;
+        return winner;
+
     }
 
 }
